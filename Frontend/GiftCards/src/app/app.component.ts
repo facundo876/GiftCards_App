2e4df4283a2630bf77c86eb938ce47f2d4b8d3ca
Facundo Arce <facundo.ea07@gmail.com>
2024-05-24 00:22:27 -0300

    add form create form gift card

--- Frontend/GiftCards/src/app/app.component.ts
+++ Frontend/GiftCards/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GiftCardCreateComponent } from "./dashboard/pages/giftcard/giftCard-Create/giftCard-Create.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, GiftCardCreateComponent]
 })
 export class AppComponent {
   title = 'GiftCards';
--- Frontend/GiftCards/src/app/dashboard/pages/giftcard/giftCard-Create/giftCard-Create.component.ts
+++ Frontend/GiftCards/src/app/dashboard/pages/giftcard/giftCard-Create/giftCard-Create.component.ts
@@ -1,14 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-gift-card-create',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
   ],
   templateUrl: './giftCard-Create.component.html',
   styleUrl: './giftCard-Create.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GiftCardCreateComponent { }
+export class GiftCardCreateComponent { 
+
+  public tipoRegalo = signal(['Producto','Monto']);
+  public selectedRegalo = signal('');
+
+  constructor() {
+    this.selectedRegalo.set(this.tipoRegalo()[0]);
+  }
+}
